4958f88de6053c901bacd7ad23c1be66a32dfcbc
Mohamed Camara GUEYE <katakeynii@gmail.com>
2021-03-23 02:24:52 +0000

    refining rails admin list

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,10 @@
 class Client < ApplicationRecord
     has_many :sequence_subscriptions, as: :user, class_name: "Sequence::Subscription"
     has_many :sequence_definitions, through: :sequence_subscriptions, class_name: "Sequence::Subscription"
+
+    rails_admin do 
+        object_label_method do 
+            :email
+        end
+    end
 end
--- app/models/sequence/subscription.rb
+++ app/models/sequence/subscription.rb
@@ -1,5 +1,34 @@
 class Sequence::Subscription < ApplicationRecord
+  after_create :create_occurences
+
   belongs_to :sequence_definition, class_name: "Sequence::Definition", foreign_key: 'sequence_definition_id'
-  belongs_to :sequence_step, class_name: "Sequence::Step", foreign_key: 'sequence_step_id'
   belongs_to :user, polymorphic: true
+
+
+
+  def create_occurences
+    self.sequence_definition.sequence_steps.each do |step|
+      Sequence::StepOccurrence.create! do |occ|
+        occ.is_sent = false 
+        occ.sequence_step = step
+        occ.sequence_subscription = self
+      end
+    end
+  end
+
+  rails_admin do
+    object_label_method do
+      :custom_label_method
+    end
+    edit do
+        field :sequence_definition
+        field :user
+    end
+  end
+
+  def custom_label_method
+    definition = self.sequence_definition.name 
+    user = self.user.email 
+    "#{definition} ->  #{user}"
+  end
 end
